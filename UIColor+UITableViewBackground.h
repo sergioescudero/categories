5d7e1ea1a49ce185b842cdec42929e36d7c17254
Sergio Escudero <sergio.escudero@me.com>
2012-12-02 11:07:20 +0100

    UIColor+UITableViewBackground

---
+++ UIColor+UITableViewBackground.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+UITableViewBackground.h
+//  U-Certificates
+//
+//  Created by Sergio Escudero Artero on 02/10/12.
+//  Copyright (c) 2012 Sergio Escudero Artero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (UITableViewBackground)
+  + (UIColor *)groupTableViewBackgroundColor;    
+@end
