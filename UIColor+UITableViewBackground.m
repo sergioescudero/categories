780dbc1902d08cf8ddb0ec13adc4d4df14aa8574
Sergio Escudero <sergio.escudero@me.com>
2013-07-22 10:39:16 +0200

    Update UIColor+UITableViewBackground.m

--- UIColor+UITableViewBackground.m
+++ UIColor+UITableViewBackground.m
@@ -1,6 +1,5 @@
 //
 //  UIColor+UITableViewBackground.m
-//  U-Certificates
 //
 //  Created by Sergio Escudero Artero on 02/10/12.
 //  Copyright (c) 2012 Sergio Escudero Artero. All rights reserved.
